bd14fd59902a04056a3bd2ab0bf1884c5d9535ef
tareqislam <blnre.tareq@gmail.com>
2022-03-07 22:30:14 +0600

    adding role table and make role crud system and role table

--- resources/views/admin/user/role/index.blade.php
+++ resources/views/admin/user/role/index.blade.php
@@ -3,18 +3,22 @@
 @section('main-content')
 <div class="row">
     <div class="col">
+
         
-        <h6 class="mb-0 text-uppercase">Striped rows</h6>
+        
+        <h6 class="mb-0 text-uppercase">All users role</h6>
         <hr>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#role_ad_modal">Add new Role</button><br><br>
         <div class="card">
             <div class="card-body">
                 <table class="table mb-0 table-striped">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -23,18 +27,13 @@
                             <td>Mark</td>
                             <td>Otto</td>
                             <td>@mdo</td>
+                            <td>
+                                <a class="btn btn-info btn-sm" href="">Edit</a>
+                                <a class="btn btn-danger btn-sm" href="">Delete</a>
+                            </td>
                         </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
+                        
+                        
                     </tbody>
                 </table>
             </div>
@@ -42,6 +41,24 @@
 
 
         
+    </div>
+</div>
+
+{{--Modal for new role--}}
+
+<div class="modal fade show" id="role_ad_modal">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur.</div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
     </div>
 </div>
 
